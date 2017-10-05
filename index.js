42e426c735eff6a3fc84505a41cc856e325b1d1c
Andrew <sbstn.andrew56@gmail.com>
2017-10-05 13:06:36 +0700

    manip number lv0-3

---
+++ index.js
@@ -0,0 +1,15 @@
+var numberA =10
+let numberB = 099
+const numberC = 0.02
+let numberE=50000
+
+let things = [100,200,300,400]
+things.push ([500,600,"dst"]);
+
+numberD= numberA/numberC
+console.log (numberD);
+
+numberF=numberE/-0.5
+console.log (numberF);
+
+console.log (things)
