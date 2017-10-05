8a290a51e17b16b51c63ab13defdd1abde3ac492
Arian <arianmarkus77@yahoo.com>
2017-10-05 10:08:23 +0700

    Manipulation number finish

---
+++ index.js
@@ -0,0 +1,88 @@
+var numberA = 10;
+let numberB = 099;
+const numberC = 0.02;
+
+console.log(numberA)
+console.log(numberB)
+console.log(numberC)
+
+
+
+
+
+// Addition
+
+
+console.log(numberA + numberC);
+
+// Subtract
+let k = numberB - numberA;
+
+console.log(k);
+
+// Multiplication
+let h = numberB * numberA;
+
+console.log(h);
+
+
+// Division
+let j = numberA / numberC;
+
+console.log(j);
+
+
+// Modulation
+let l = numberA % numberC;
+
+console.log(l);
+
+//task level2
+
+let x1 = -10;
+let y1 = 0.02;
+let a = x1 + y1; // Addition
+
+console.log(a);
+
+let x2 = -10;
+let y2 = 0.02;
+let b = x2 - y2; // substract
+
+console.log(b);
+
+let x3= 1000;
+let y3= 2000;
+let c = x3 - y3 ; // Multiplication
+
+console.log(c);
+
+let x4 = 10000;
+let y4 = 500;
+let d = x4 / y4 ; // Division
+
+console.log(d);
+
+let x5 = -2000;
+let y5 = -500;
+let e = x5 % y5; // Modulation
+
+console.log(e);
+
+let things = [100,200,300,400,500]
+
+things.push('True')
+
+console.log(things);
+
+things.push('Roti','Pisang')
+console.log(things);
+things.concat(200,'Roti')
+console.log(things);
+things.pop('Pisang')
+console.log(things);
+
+
+for (var i = 0; i < things.length; i++) {
+    console.log(things[i])
+}
